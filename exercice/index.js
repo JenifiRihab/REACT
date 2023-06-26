084b3d0399f235ab43a6369c8627cd4b50d9441e
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-06-26 15:25:54 +0200

    go to the REACT

--- exercice/index.js
+++ exercice/index.js
@@ -42,26 +42,36 @@
 // _____________________________
 
 
-// Consigne :
+// EXERCICES :
 // en utilisant le code de l'exo précédent, refactorisez votre javascript :
 // -- créez une fonction réutilisable "createNodeElement" qui permettra de créer n'importe quel élément lorsqu'elle est appelée. 
 // Pour cela vous devez passez des parametres à votre fonction (tagType, className, text et imgSrc). Certains parametres doivent être optionnels pour que votre fonction marche dans n'importe quel cas. Pensez à utiliser le return dans votre fonction pour pouvoir récupérer l'élément créé quand la fonction est appelée. L'élément récupérée pourra ainsi être inséré dans votre dom.
 
 
+
+
+// SOLUTION:
 const button = document.querySelector("button");
+
+// au clicksur le button:
     button.addEventListener("click", () => {
-    
+
+// je sible l'element dans le DOM quI a l'ID travel:    
     const divRoot = document.querySelector("#travel");
 
+ // je créé une bolise article:   
     const articleElement = createNodeElement("article", "article-travel");
     divRoot.appendChild(articleElement);
 
+  // créer un titre :  
     const titleElement = createNodeElement("h1", "title-travel", "COME SEE THE WORLD ");
     articleElement.appendChild(titleElement);
 
+ // créer une image:
     const imgElement = createNodeElement( "img", "img-travel", "", "../exercice/img/photos-de-voyage-piscine-debordement-hotel-reve.jpg");
     articleElement.appendChild(imgElement);
 
+ // créer un text:
     const textElement = createNodeElement("p", "text-travel", "Imaginez un peignoir jeté sur une chaise longue savamment placée aux abords de la piscine d’un hôtel chic, dont la terrasse surplombe la mer… D’ailleurs, n’hésitez pas à jeter un oeil à ces piscines de rêve autour du monde.");
     articleElement.appendChild(textElement);
 });
